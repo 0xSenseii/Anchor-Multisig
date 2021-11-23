06b89b5e12113f7e57769f7a2c0c1e3b3ca90178
Carlos Gonzalez <carlosgj94@gmail.com>
2021-11-23 21:59:42 +0000

    BREAKING CHANGE: account not being stored

--- tests/Goldmandao-Multisig.ts
+++ tests/Goldmandao-Multisig.ts
@@ -1,20 +1,20 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { GoldmandaoMultisig } from '../target/types/goldmandao_multisig';
+import assert from "assert";
+
 const { SystemProgram } = anchor.web3;
 
 describe('GoldmanDAO-Multisig', () => {
 
   // Configure the client to use the local cluster.
-  const provider = anchor.Provider.env();
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.local();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.GoldmandaoMultisig as Program<GoldmandaoMultisig>;
-  const myAccount = anchor.web3.Keypair.generate();
-  const myAccount2 = anchor.web3.Keypair.generate();
 
   it('Is initialized!', async () => {
-    // Add your test here.
+    const program = anchor.workspace.GoldmandaoMultisig as Program<GoldmandaoMultisig>;
+    const myAccount = anchor.web3.Keypair.generate();
     const tx = await program.rpc.initialize(
         new anchor.BN(1234),
         {
@@ -27,18 +27,7 @@ describe('GoldmanDAO-Multisig', () => {
         }
     );
     console.log("Your transaction signature", tx);
-
-    const tx2 = await program.rpc.initialize(
-        new anchor.BN(4321),
-        {
-            accounts: {
-                myAccount: myAccount2.publicKey,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            },
-            signers: [myAccount2],
-        }
-    );
-    console.log("Your transaction signature", tx2);
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.ok(account.data.eq(new anchor.BN(1234)));
   });
 });
