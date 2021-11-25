119405682f9915835c57ded37a7c5636c9a0c810
Carlos Gonzalez <carlosgj94@gmail.com>
2021-11-25 13:05:18 +0000

    feat: create a dao token with some permisions

--- programs/goldmandao-multisig/src/lib.rs
+++ programs/goldmandao-multisig/src/lib.rs
@@ -1,52 +1,92 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, MintTo, Token, Mint};
 
 declare_id!("D3ogHpj4ZXexUjWCo2Y1PKZsKVHPY7x4whCCCyq5kHsx");
 
-// Every method under the #program attribute is an entrypoint to the program that clients may invoke.
+const DECIMALS: u8 = 6;
+
 #[program]
 pub mod goldmandao_multisig {
     use super::*;
-    // Context is a struct containning the program_id with Generic type
-    // In this case Initialize is the type of Context
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+
+    pub fn initialize_dao(
+        ctx: Context<InitializeDao>,
+        dao_name: String,
+        bumps: PoolBumps,
+        num_tokens: u64,
+    ) -> ProgramResult {
+        msg!("INIT DAO");
         Ok(())
     }
-    pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+
+    pub fn mint_tokens(ctx: Context<MintOwnership>) -> ProgramResult {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.dao_token.to_account_info().clone(),
+            to: ctx.accounts.member.to_account_info().clone(),
+            authority: ctx.accounts.authority.to_account_info().clone(),
+        };
+
+        // let seeds = &[ctx.accounts.signer.as_ref(), &[self.nonce]];
+        // let signer = &[&seeds[..]];
+
+        let amount = 100;
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("INIT MINTING");
+        token::mint_to(cpi_context, amount)?;
         Ok(())
     }
 }
 
-// Accounts on Solana work as files on an OS, they store data of a tx.
-
-/* The account includes metadata for the lifetime of the file. That lifetime
-is expressed by a number of fractional native tokens called lamports.
-Accounts are held in validator memory and pay "rent" to stay there.
-Each validator periodically scans all accounts and collects rent.
-Any account that drops to zero lamports is purged.
-Accounts can also be marked rent-exempt if they contain a sufficient number of lamports.
-*/
-
-// This macro is a struct of all accounts in our program
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub my_account: Account<'info, MyAccount>,
+#[instruction(dao_name: String, bumps: PoolBumps)]
+pub struct InitializeDao<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub dao_authority: Signer<'info>,
+    // DAO Account
+    #[account(init,
+        seeds = [dao_name.as_bytes()],
+        bump = bumps.dao_account,
+        payer = dao_authority)]
+    pub dao_account: Box<Account<'info, DAOAccount>>,
+
+    #[account(init,
+        mint::decimals = DECIMALS,
+        mint::authority = dao_account,
+        seeds = [dao_name.as_bytes(), b"redeemable_mint".as_ref()],
+        bump = bumps.redeemable_mint,
+        payer = dao_authority)]
+    pub redeemable_mint: Box<Account<'info, Mint>>,
+
     pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+pub struct MintOwnership<'info> {
+    pub authority: AccountInfo<'info>,
+    #[account(signer)]
+    pub signer: AccountInfo<'info>,
     #[account(mut)]
-    pub my_account: Account<'info, MyAccount>,
+    pub member: AccountInfo<'info>,
+    #[account(mut)]
+    dao_token: AccountInfo<'info>,
+    token_program: AccountInfo<'info>,
 }
 
 #[account]
-pub struct MyAccount {
-    pub data: u64,
+#[derive(Default)]
+pub struct DAOAccount {
+    pub dao_name: [u8; 10], // Setting an arbitrary max of ten characters in the ido name.
+    pub bumps: PoolBumps,
+    pub dao_authority: Pubkey,
+    pub redeemable_mint: Pubkey,
+    pub num_dao_tokens: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Default, Clone)]
+pub struct PoolBumps {
+    pub dao_account: u8,
+    pub redeemable_mint: u8,
 }
--- tests/Goldmandao-Multisig.ts
+++ tests/Goldmandao-Multisig.ts
@@ -1,9 +1,11 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+
 import { GoldmandaoMultisig } from '../target/types/goldmandao_multisig';
 import assert from "assert";
 
-const { SystemProgram } = anchor.web3;
+// const { SystemProgram } = anchor.web3;
 
 describe('GoldmanDAO-Multisig', () => {
 
@@ -11,23 +13,70 @@ describe('GoldmanDAO-Multisig', () => {
   const provider = anchor.Provider.local();
   anchor.setProvider(provider);
 
+  const signer = anchor.web3.Keypair.generate()
+  const program = anchor.workspace.GoldmandaoMultisig as Program<GoldmandaoMultisig>;
+
+  // All mints default to 6 decimal places.
+  const daoTokenAmount = new anchor.BN(5000000)
+  const daoName = 'test_dao'
 
   it('Is initialized!', async () => {
-    const program = anchor.workspace.GoldmandaoMultisig as Program<GoldmandaoMultisig>;
-    const myAccount = anchor.web3.Keypair.generate();
-    const tx = await program.rpc.initialize(
-        new anchor.BN(1234),
-        {
-            accounts: {
-                myAccount: myAccount.publicKey,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            },
-            signers: [myAccount],
-        }
+    // We give money to the "signer"
+    await requestAirdrop({provider, pubKey: signer.publicKey});
+    let bumps = new PoolBumps();
+
+    // We need the program as authority so it can tranfer funds
+    const [daoAccount, daoAccountBump] = await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(daoName)],
+        program.programId
+    )
+    bumps.daoAccount = daoAccountBump
+
+    const [redeemableMint, redeemableMintBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from(daoName), Buffer.from("redeemable_mint")],
+      program.programId
     );
-    console.log("Your transaction signature", tx);
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
-    assert.ok(account.data.eq(new anchor.BN(1234)));
+    bumps.redeemableMint = redeemableMintBump;
+
+    const tx = await program.rpc.initializeDao(
+      daoName,
+      bumps,
+      daoTokenAmount,
+      {
+        accounts: {
+          daoAuthority: provider.wallet.publicKey,
+          daoAccount,
+          redeemableMint,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      }
+    )
+    console.log("Transaction is: ", tx)
   });
+  function PoolBumps() {
+    this.daoAccount;
+    this.redeemableMint;
+  };
 });
+
+
+const createToken = async ({ provider, wallet, mintAuthority }) =>
+  Token.createMint(
+    provider.connection,
+    wallet,
+    mintAuthority,
+    null,
+    8,
+    TOKEN_PROGRAM_ID
+  )
+
+
+const requestAirdrop = async ({ provider, pubKey }) =>
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(pubKey, 10000000000),
+      "confirmed"
+    );
+
+
